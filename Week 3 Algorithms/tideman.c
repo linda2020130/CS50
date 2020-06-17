fad6f4185e2bfd50d93158c7140605cc7a5d2fda
linda2020130 <60461483+linda2020130@users.noreply.github.com>
2020-06-17 21:07:32 +0800

    Update tideman.c

--- Week 3 Algorithms/tideman.c
+++ Week 3 Algorithms/tideman.c
@@ -1,3 +1,15 @@
+/*
+Implement a program that runs a Tideman election.
+The Tideman voting method consists of three parts:
+1. Tally: Once all of the voters have indicated all of their preferences, determine, for each pair of candidates, 
+who the preferred candidate is and by what margin they are preferred.
+2. Sort: Sort the pairs of candidates in decreasing order of strength of victory, 
+where strength of victory is defined to be the number of voters who prefer the preferred candidate.
+3. Lock: Starting with the strongest pair, go through the pairs of candidates in order and 
+“lock in” each pair to the candidate graph, so long as locking in that pair does not create a cycle in the graph.
+Once the graph is complete, the source of the graph (the one with no edges pointing towards it) is the winner!
+*/
+
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
